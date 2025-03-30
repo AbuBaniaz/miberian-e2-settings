e37554fd2f3e105d815cb6758642a366682571fe
miberian <97098684+miberian@users.noreply.github.com>
2025-03-30 16:02:11 +0200

    Simplyflied disable IPv6

--- install/custom-e2.sh
+++ install/custom-e2.sh
@@ -8,3 +8,14 @@ A=$$; ( wget -q $lamedb_origin -O /tmp/$A.d && mv /tmp/$A.d /etc/enigma2/lamedb 
 # Clean default bouquet settings
 rm /etc/enigma2/bouquets.*
 rm /etc/enigma2/userbouquet.favourites.*
+
+# Disable IPv6
+
+sed -i '/^[^#]/ s/\(^.*inet6.*$\)/#\ \1/' /etc/network/interfaces
+
+printf '
+# Disable IPv6
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+' >> /etc/sysctl.conf
