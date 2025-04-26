95e9003f02fa7cfbdd9a9f6d0d7f37eee603f37f
miberian <97098684+miberian@users.noreply.github.com>
2025-04-26 19:30:11 +0200

    Add additional provider

--- gbue4k/openatv/usr/script/services_restart.sh
+++ gbue4k/openatv/usr/script/services_restart.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+update_channel_list () {
+
+cat /etc/enigma2/userbouquet.abm.sat_192_sky_deutschland.main.tv | grep :85:c00000:0:0:0: | cut -d " " -f 2 | awk '{print toupper($0)}' > /etc/enigma2/whitelist_streamrelay
+
+}
+
 # Check for standby
 standby=$(wget -O- -q http://127.0.0.1/api/powerstate | grep instandby | cut -d ":" -f 2)
 
 # Check for number of active recordings
 recordings=$(wget -O- -q http://127.0.0.1/api/timerlist | grep '"state": 2' | wc -l)
+
+# Update channel list
+update_channel_list
 
 if [ "$standby" = " true" ] && [ "$recordings" = "0" ]; then
     # Restart enigma2
